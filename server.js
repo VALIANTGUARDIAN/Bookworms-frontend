e051508b2f8343576d38abb19c854731d534c56b
DIGVIJAY <singhdigvijay703@gmail.com>
2021-06-24 11:07:09 +0530

    ..

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ const express = require('express');
 const app = express();
 app.use(requireHTTPS);
 
-app.use(express.static('./dist/frontend'));
+app.use(express.static('./dist/bookworms'));
 
 app.get('/*', function (req, res) {
-    res.sendFile('index.html', { root: 'dist/frontend/' }
+    res.sendFile('index.html', { root: 'dist/bookworms/' }
     );
 });
 
